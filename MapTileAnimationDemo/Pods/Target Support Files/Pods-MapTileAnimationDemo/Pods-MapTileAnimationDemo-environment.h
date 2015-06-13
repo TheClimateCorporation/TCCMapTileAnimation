4f72c16d324c024f73fb560853b9815fc8ee9dae
Richard Shin <richard@richardshin.com>
2015-06-13 12:37:47 -0700

    Update podspec and demo project to 0.4.10

--- MapTileAnimationDemo/Pods/Target Support Files/Pods-MapTileAnimationDemo/Pods-MapTileAnimationDemo-environment.h
+++ MapTileAnimationDemo/Pods/Target Support Files/Pods-MapTileAnimationDemo/Pods-MapTileAnimationDemo-environment.h
@@ -10,5 +10,5 @@
 #define COCOAPODS_POD_AVAILABLE_TCCMapTileAnimation
 #define COCOAPODS_VERSION_MAJOR_TCCMapTileAnimation 0
 #define COCOAPODS_VERSION_MINOR_TCCMapTileAnimation 4
-#define COCOAPODS_VERSION_PATCH_TCCMapTileAnimation 8
+#define COCOAPODS_VERSION_PATCH_TCCMapTileAnimation 10
 
